d84933658a335b6fd663312fb4381b6f7499efb2
juanmigar <juangarciaborda8@gmail.com>
2025-05-05 20:25:12 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,35 @@
 variable "client_id" {
   type        = string
-  description = "ID del cliente de la app registrada en Azure"
+  description = "0390f67a-1000-49b0-b928-8db8d915be00"
 }
 
 variable "tenant_id" {
   type        = string
-  description = "ID del tenant de Azure"
+  description = "8d8d0da4-7a1b-4b8a-9900-1cb56400e52f"
 }
 
 variable "subscription_id" {
   type        = string
-  description = "ID de la suscripción de Azure"
+  description = "d19716ea-6e79-435a-b563-2221ae09833f"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "vnet-demo"
+}
+
+variable "subnet1_name" {
+  type        = string
+  description = "subnet1"
+}
+
+variable "subnet2_name" {
+  type        = string
+  description = "subnet2"
+}
+
+variable "location" {
+  type        = string
+  description = "Ubicación del grupo de recursos"
+  default     = "East US"
 }
