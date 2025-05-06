1270cbf1c6b58d77203740e247e2b7b99190fefb
juanmigar <juangarciaborda8@gmail.com>
2025-05-05 20:07:22 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,8 @@
 provider "azurerm" {
   features {}
-}
-variable "client_id" {
-  type        = string
-  description = "ID de la aplicación (cliente) en Azure AD"
-}
 
-variable "tenant_id" {
-  type        = string
-  description = "ID del inquilino (tenant) en Azure AD"
-}
-
-variable "subscription_id" {
-  type        = string
-  description = "ID de la suscripción de Azure"
+  use_oidc        = true
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
