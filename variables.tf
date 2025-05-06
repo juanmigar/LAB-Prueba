38bd36544781a462f53dc699e6909cc490ca280e
juanmigar <juangarciaborda8@gmail.com>
2025-05-05 20:08:16 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
-variable "client_id" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
+variable "client_id" {
+  type        = string
+  description = "ID del cliente de la app registrada en Azure"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "ID del tenant de Azure"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "ID de la suscripción de Azure"
+}
