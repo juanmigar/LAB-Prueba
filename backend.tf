678d87f28cb2474c51a1618d07277ae5e4b18474
juanmigar <juangarciaborda8@gmail.com>
2025-05-08 11:31:00 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "rg-tfstate"           # El nombre de tu grupo de recursos existente
-    storage_account_name  = "tfsatestorage001"            # El nombre de tu cuenta de almacenamiento existente (en minúsculas y entre 3 y 24 caracteres)
-    container_name        = "tfstate"                   # El nombre de tu contenedor existente dentro de la cuenta de almacenamiento
+    resource_group_name   = "my-rg"           # El nombre de tu grupo de recursos existente
+    storage_account_name  = "mystorageaccjuanito999"            # El nombre de tu cuenta de almacenamiento existente (en minúsculas y entre 3 y 24 caracteres)
+    container_name        = "tfstatesoo"                   # El nombre de tu contenedor existente dentro de la cuenta de almacenamiento
     key                   = "terraform.tfstate"         # El nombre del archivo de estado (puedes dejarlo igual o cambiarlo)
   }
 }
